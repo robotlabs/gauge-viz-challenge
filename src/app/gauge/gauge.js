11732de29421b028cfa24b8d3a1e8f1fb8b2d4a6
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2018-10-31 13:29:33 +0000

    start creating d3 dataviz

--- src/app/gauge/gauge.js
+++ src/app/gauge/gauge.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import * as d3 from "d3";
 import './style.css';
 
 class Gauge extends Component{
   constructor(props) {
     super(props);
+    //** ref to select svg with d3 */
     this.svgD = React.createRef();
+  }
+  componentDidMount() {
+    //** create svg container */
+    this.svg = d3.select(this.svgD.current)
+      .append('g')
+      .attr('transform', 'translate(' + 0 + ',' + 0 + ')');
   }
   render() {
     return (
