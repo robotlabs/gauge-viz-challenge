b6eaf0d79ed36b150b0c7f9077c7817ece1239c1
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2018-10-31 13:56:16 +0000

    build d3 (empty) elements

--- src/app/App.js
+++ src/app/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import {gaugeApi} from './../services/api';
 import {gaugeParser} from './../services/data-parser';
+import Gauge from './gauge/gauge'
 import './app.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //** init state */
+    this.state = {
+      gaugeData: {}
+    }
+  }
   componentDidMount() {
     //** test api */
     gaugeApi(gaugeParser)
       .then((data) => {
-        //returning data after parse
+        //returning data after parse ()
         console.log('data from api test', data);
       })
   }
   render() {
+    console.log('this state ', this.state);
     return (
-      <div className="app">
-        Ciao
+      <div className="appx">
+        <Gauge
+          data={this.state.gaugeData}>
+        </Gauge>
       </div>
     );
   }
--- src/app/gauge/gauge.js
+++ src/app/gauge/gauge.js
@@ -3,6 +3,11 @@ import * as d3 from "d3";
 import * as utils from './utils.js';
 import './style.css';
 
+//** init params */
+const pi = Math.PI; 
+const radius = 80;
+const innerRadius = radius - (radius / 2.1);
+
 class Gauge extends Component{
   constructor(props) {
     super(props);
@@ -19,6 +24,24 @@ class Gauge extends Component{
     this.copyMin = utils.createText(-80, 10, 8, this.svg);
     this.copyMax = utils.createText(80, 10, 8, this.svg);
     this.copyValue = utils.createText(0, -5, 11, this.svg);
+
+    //** create arc */
+    this.arc = d3.arc()
+      .innerRadius(innerRadius)
+      .outerRadius(radius)
+      .startAngle(-pi / 2);
+
+    //** background arc path*/
+    this.svg.append("path")
+      .datum({endAngle: pi / 2})
+      .style("fill", "#ddd")
+      .attr("d", this.arc);
+
+    //** percentageArc arc path*/
+    this.percentageArc = this.svg.append("path")
+      .datum({endAngle: -pi / 2})
+      .style("fill", "#ff0099")
+      .attr("d", this.arc);
   }
   render() {
     return (
