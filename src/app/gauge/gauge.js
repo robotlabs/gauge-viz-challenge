14f2c5c5d891d002dc7ae73905c84aebef279ca5
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2018-10-31 12:59:35 +0000

    added svg (d3 container) in gauge

--- src/app/gauge/gauge.js
+++ src/app/gauge/gauge.js
@@ -2,10 +2,20 @@ import React, {Component} from 'react';
 import './style.css';
 
 class Gauge extends Component{
+  constructor(props) {
+    super(props);
+    this.svgD = React.createRef();
+  }
   render() {
     return (
-      <div className="">
-        ciao gauge
+      <div
+        className="gauge">
+        <svg  
+          className="svg-container"
+          viewBox="0 0 200 200"
+          preserveAspectRatio="xMidYMid"
+          ref={this.svgD}>
+        </svg>
       </div>
     );
   }
