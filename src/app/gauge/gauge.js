ca9d07c282a2578cabca8d1e0862361effc226d0
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2018-10-31 12:56:05 +0000

    added empty gauge component

---
+++ src/app/gauge/gauge.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './style.css';
+
+class Gauge extends Component{
+  render() {
+    return (
+      <div className="">
+        ciao gauge
+      </div>
+    );
+  }
+}
+
+export default Gauge;
