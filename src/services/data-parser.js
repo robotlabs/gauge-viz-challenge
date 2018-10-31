ce18a2887444ea097d4519edf0ead90262cc8e45
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2018-10-31 14:36:39 +0000

    add symbols in the parser after data fetched

--- src/services/data-parser.js
+++ src/services/data-parser.js
@@ -1,3 +1,11 @@
+import CurrencyInfo from './../data/currency-info.json'
 export const gaugeParser = (d) => {
+  let currencyString = '';
+  console.log('dxx ', d);
+  if (d.format === 'currency') {
+    currencyString = CurrencyInfo[d.unit].symbol;
+  }
+  d.minCopy = currencyString + d.min;
+  d.maxCopy = currencyString + d.max;
   return d;
 }
